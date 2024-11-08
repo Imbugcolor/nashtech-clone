abca966b6b7fdc6415aaca1c7b6589726b64157d
Imbugcolor <viethd123456@gmail.com>
2024-11-08 16:26:23 +0700

    URL config

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,1 +1,2 @@
-export const SERVER_URL = 'http://localhost:3001'
+// export const SERVER_URL = 'http://localhost:3001' // test in local
+export const SERVER_URL = 'https://nashtech-clone.onrender.com' // in deployment url
